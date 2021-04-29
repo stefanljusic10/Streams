9e73569c9c80244f173bd9329b76b8bcdb044e1b
stefanljusic10 <53339429+stefanljusic10@users.noreply.github.com>
2021-04-30 00:43:37 +0200

    auth status

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,19 +1,31 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 function GoogleAuth() {
 
+    const [isSignedIn, setIsSignedIn] = useState(null)
+
     useEffect(() => {
-        window.gapi.load('client:auth2', ()=>{
+        window.gapi.load('client:auth2', () => {
             window.gapi.client.init({
                 clientId: '630859611893-v05fsi56pgj6il81lpcm8pgfjsva3k4n.apps.googleusercontent.com',
                 scope: 'email'
             })
+                .then(() => {
+                    const auth = window.gapi.auth2.getAuthInstance()
+                    setIsSignedIn(auth.isSignedIn.get())
+                })
         })
     }, [])
 
+    const renderAuthButton = 
+        isSignedIn === null ? 
+        <div>Idk</div> : 
+        isSignedIn ? 
+        <div>Yes</div> : <div>No</div>
+
     return (
         <div className="header-nav-item">
-            Google Auth
+            {renderAuthButton}
         </div>
     )
 }
