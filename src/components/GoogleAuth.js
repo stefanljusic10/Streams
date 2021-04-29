12506c2e595b125dcadf395d2395925ebaca62c0
stefanljusic10 <53339429+stefanljusic10@users.noreply.github.com>
2021-04-30 00:19:31 +0200

    wiring google api library

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 function GoogleAuth() {
+
+    useEffect(() => {
+        window.gapi.load('client:auth2', ()=>{
+            window.gapi.client.init({
+                clientId: '630859611893-v05fsi56pgj6il81lpcm8pgfjsva3k4n.apps.googleusercontent.com',
+                scope: 'email'
+            })
+        })
+    }, [])
+
     return (
         <div className="header-nav-item">
             Google Auth
