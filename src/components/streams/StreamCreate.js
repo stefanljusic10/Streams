e12c2bae3965809d14b724dfed7ed9724612935e
stefanljusic10 <53339429+stefanljusic10@users.noreply.github.com>
2021-05-02 22:30:53 +0200

    formik - create new stream

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,9 +1,35 @@
 import React from 'react'
+import { useFormik } from 'formik'
 
 function StreamCreate() {
+
+    const formik = useFormik({
+        initialValues: {
+            title: '',
+            description: ''
+        },
+        onSubmit: values => {
+            console.log(values);
+        }
+    })
+
     return (
         <div>
-            Stream Create
+            <form onSubmit={formik.handleSubmit}>
+                <input
+                    type="text" name="title" placeholder="Title"
+                    onChange={formik.handleChange}
+                    value={formik.values.title}
+                />
+                <br /><br />
+                <input
+                    type="text" name="description" placeholder="Description"
+                    onChange={formik.handleChange}
+                    value={formik.values.description}
+                />
+                <br /><br />
+                <button type="submit">Create new Stream</button>
+            </form>
         </div>
     )
 }
