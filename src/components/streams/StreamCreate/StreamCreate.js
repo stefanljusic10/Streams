4bc61e2e9f00ccc4569d088c70661d95d33f7420
stefanljusic10 <53339429+stefanljusic10@users.noreply.github.com>
2021-05-05 18:18:16 +0200

    refactoring formik

--- src/components/streams/StreamCreate/StreamCreate.js
+++ src/components/streams/StreamCreate/StreamCreate.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { useFormik } from 'formik'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
+import * as Yup from 'yup'
 import './StreamCreate.css'
 
 function StreamCreate() {
@@ -13,56 +14,37 @@ function StreamCreate() {
         console.log(values);
     }
 
-    const validate = values => {
-        let errors = {}
-
-        if (!values.title) {
-            errors.title = 'Required!'
-        }
-
-        if (!values.description) {
-            errors.description = 'Required!'
-        }
-
-        return errors
-    }
-
-    const formik = useFormik({
-        initialValues,
-        onSubmit,
-        validate
+    const validationSchema = Yup.object({
+        title: Yup.string().required('Required!'),
+        description: Yup.string().required('Required!')
     })
 
-    console.log(formik.errors);
-
     return (
-        <div>
-            <form className="form-formik" onSubmit={formik.handleSubmit}>
-                <input
+        <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={onSubmit}
+        >
+            <Form className="form-formik">
+                <Field
                     type="text" name="title" placeholder="Title"
                     className="input-field"
-                    onBlur={formik.handleBlur}
-                    onChange={formik.handleChange}
-                    value={formik.values.title}
                 />
-                {formik.touched.title && formik.errors.title ? 
-                <div className="form-error">{formik.errors.title}</div> : null}
+                <ErrorMessage name='title' />
 
                 <br /><br />
-                <textarea
+                <Field
+                    as='textarea'
                     type="text" name="description" placeholder="Description"
                     className="input-field"
                     rows="7"
-                    onBlur={formik.handleBlur}
-                    onChange={formik.handleChange}
-                    value={formik.values.description}
                 />
-                {formik.touched.description && formik.errors.description ? <div className="form-error">{formik.errors.description}</div> : null}
+                <ErrorMessage name='description' />
 
                 <br /><br />
                 <button type="submit">Create new Stream</button>
-            </form>
-        </div>
+            </Form>
+        </Formik>
     )
 }
 
